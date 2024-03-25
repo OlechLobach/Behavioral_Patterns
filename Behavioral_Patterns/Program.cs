e51dd7d35b55f859faab8d9d490d15f9c6cf3f56
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-25 17:20:21 +0200

    initiall commit

--- Behavioral_Patterns/Program.cs
+++ Behavioral_Patterns/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Behavioral_Patterns
+﻿using CommandPattern;
+
+namespace MainProject
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Editor editor = new Editor();
+
+            ICommand addCommand = new AddTextCommand(editor, "Hello");
+            ICommand deleteCommand = new DeleteTextCommand(editor, "Hello");
+            ICommand selectCommand = new SelectTextCommand(editor, "World");
+
+            editor.ExecuteCommand(addCommand);
+            editor.ExecuteCommand(deleteCommand);
+            editor.ExecuteCommand(selectCommand);
+
+            editor.UndoCommand(selectCommand);
+            editor.UndoCommand(deleteCommand);
+            editor.UndoCommand(addCommand);
         }
     }
 }
+
+
+
+
---
+++ Patterns/Command/CommabdProj.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    public interface ICommand
+    {
+        void Execute();
+        void Undo();
+    }
+
+    public class AddTextCommand : ICommand
+    {
+        private readonly Editor _editor;
+        private readonly string _text;
+
+        public AddTextCommand(Editor editor, string text)
+        {
+            _editor = editor;
+            _text = text;
+        }
+
+        public void Execute()
+        {
+            _editor.AddText(_text);
+        }
+
+        public void Undo()
+        {
+            _editor.DeleteText(_text);
+        }
+    }
+
+    public class DeleteTextCommand : ICommand
+    {
+        private readonly Editor _editor;
+        private readonly string _text;
+
+        public DeleteTextCommand(Editor editor, string text)
+        {
+            _editor = editor;
+            _text = text;
+        }
+
+        public void Execute()
+        {
+            _editor.DeleteText(_text);
+        }
+
+        public void Undo()
+        {
+            _editor.AddText(_text);
+        }
+    }
+
+    public class SelectTextCommand : ICommand
+    {
+        private readonly Editor _editor;
+        private readonly string _text;
+
+        public SelectTextCommand(Editor editor, string text)
+        {
+            _editor = editor;
+            _text = text;
+        }
+
+        public void Execute()
+        {
+            _editor.SelectText(_text);
+        }
+
+        public void Undo()
+        {
+            _editor.DeselectText(_text);
+        }
+    }
+
+    public class Editor
+    {
+        private readonly List<string> _text = new List<string>();
+        private readonly List<string> _selectedText = new List<string>();
+
+        public void AddText(string text)
+        {
+            _text.Add(text);
+            Console.WriteLine($"Added text: {text}");
+        }
+
+        public void DeleteText(string text)
+        {
+            _text.Remove(text);
+            Console.WriteLine($"Deleted text: {text}");
+        }
+
+        public void SelectText(string text)
+        {
+            _selectedText.Add(text);
+            Console.WriteLine($"Selected text: {text}");
+        }
+
+        public void DeselectText(string text)
+        {
+            _selectedText.Remove(text);
+            Console.WriteLine($"Deselected text: {text}");
+        }
+
+        public void ExecuteCommand(ICommand command)
+        {
+            command.Execute();
+        }
+
+        public void UndoCommand(ICommand command)
+        {
+            command.Undo();
+        }
+    }
+}
