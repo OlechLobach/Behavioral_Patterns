9336ccf6af857ccd8912ff8a1b2bba8e542d3b9d
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-25 17:32:57 +0200

    initiall commit

--- Behavioral_Patterns/Program.cs
+++ Behavioral_Patterns/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Behavioral_Patterns
+﻿using MediatorPattern;
+
+
+namespace MainProject { 
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        ChatRoom chatMediator = new ChatRoom();
+
+        User user1 = new ChatUser(chatMediator, "User 1");
+        User user2 = new ChatUser(chatMediator, "User 2");
+        User user3 = new ChatUser(chatMediator, "User 3");
+
+        chatMediator.AddUser(user1);
+        chatMediator.AddUser(user2);
+        chatMediator.AddUser(user3);
+
+        user1.Send("Hello, everyone!");
+        user2.Send("Hi, User 1!");
+        user3.Send("Hey there!");
     }
+}
 }
---
+++ Patterns/Mediator/Chat.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorPattern
+{
+    public abstract class ChatMediator
+    {
+        public abstract void SendMessage(string message, User user);
+    }
+
+    public class ChatRoom : ChatMediator
+    {
+        private List<User> users = new List<User>();
+
+        public override void SendMessage(string message, User user)
+        {
+            foreach (var u in users)
+            {
+                if (u != user)
+                {
+                    u.Receive(message);
+                }
+            }
+        }
+
+        public void AddUser(User user)
+        {
+            users.Add(user);
+        }
+    }
+
+    public abstract class User
+    {
+        protected ChatMediator mediator;
+        protected string name;
+
+        public User(ChatMediator mediator, string name)
+        {
+            this.mediator = mediator;
+            this.name = name;
+        }
+
+        public abstract void Send(string message);
+        public abstract void Receive(string message);
+    }
+
+    public class ChatUser : User
+    {
+        public ChatUser(ChatMediator mediator, string name) : base(mediator, name)
+        {
+        }
+
+        public override void Send(string message)
+        {
+            Console.WriteLine($"{name} sends: {message}");
+            mediator.SendMessage(message, this);
+        }
+
+        public override void Receive(string message)
+        {
+            Console.WriteLine($"{name} receives: {message}");
+        }
+    }
+}
