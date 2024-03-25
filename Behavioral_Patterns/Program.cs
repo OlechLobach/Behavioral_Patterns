c3cbce032d698d70ed02e86961081d1066e1e57c
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-25 17:25:40 +0200

    initiall commit

--- Behavioral_Patterns/Program.cs
+++ Behavioral_Patterns/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Behavioral_Patterns
+﻿using ChainOfResponsibilityPattern;
+
+
+namespace MainProject { 
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        PaymentHandler fundsHandler = new FundsAvailabilityHandler();
+        PaymentHandler smsHandler = new SmsConfirmationHandler();
+        PaymentHandler appHandler = new AppConfirmationHandler();
+
+        fundsHandler.SetSuccessor(smsHandler);
+        smsHandler.SetSuccessor(appHandler);
+
+        PaymentRequest request1 = new PaymentRequest { Amount = 500 };
+        fundsHandler.HandlePayment(request1);
+        Console.WriteLine($"Payment 1 confirmed: {request1.IsConfirmed}");
+
+        PaymentRequest request2 = new PaymentRequest { Amount = 5000 };
+        fundsHandler.HandlePayment(request2);
+        Console.WriteLine($"Payment 2 confirmed: {request2.IsConfirmed}");
+
+        PaymentRequest request3 = new PaymentRequest { Amount = 10000 };
+        fundsHandler.HandlePayment(request3);
+        Console.WriteLine($"Payment 3 confirmed: {request3.IsConfirmed}");
     }
+}
 }
---
+++ Patterns/ChainOfResponsibilityPattern/Payment.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibilityPattern
+{
+
+    public class PaymentRequest
+    {
+        public decimal Amount { get; set; }
+        public bool IsConfirmed { get; set; }
+    }
+
+    public abstract class PaymentHandler
+    {
+        protected PaymentHandler Successor;
+
+        public void SetSuccessor(PaymentHandler successor)
+        {
+            Successor = successor;
+        }
+
+        public abstract void HandlePayment(PaymentRequest request);
+    }
+
+    public class FundsAvailabilityHandler : PaymentHandler
+    {
+        public override void HandlePayment(PaymentRequest request)
+        {
+            if (request.Amount <= 1000)
+            {
+                Console.WriteLine("Checking funds availability...");
+                request.IsConfirmed = true;
+            }
+            else if (Successor != null)
+            {
+                Successor.HandlePayment(request);
+            }
+        }
+    }
+
+    public class SmsConfirmationHandler : PaymentHandler
+    {
+        public override void HandlePayment(PaymentRequest request)
+        {
+            if (request.Amount <= 5000)
+            {
+                Console.WriteLine("Sending SMS confirmation...");
+                request.IsConfirmed = true;
+            }
+            else if (Successor != null)
+            {
+                Successor.HandlePayment(request);
+            }
+        }
+    }
+
+    public class AppConfirmationHandler : PaymentHandler
+    {
+        public override void HandlePayment(PaymentRequest request)
+        {
+            Console.WriteLine("Sending app confirmation...");
+            request.IsConfirmed = true;
+        }
+    }
+}
